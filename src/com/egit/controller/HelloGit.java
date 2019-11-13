b4cdc59afdb3f425e8a3be1ca9dfb3cc645c184d
WalkerWei <319230300@qq.com>
2019-11-13 08:37:02 +0800

    2.0

--- src/com/egit/controller/HelloGit.java
+++ src/com/egit/controller/HelloGit.java
@@ -3,6 +3,9 @@ package com.egit.controller;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit");
+		System.out.println("HelloGit");
+		System.out.println("HelloGit");
+		System.out.println("HelloGit");
 		
 	}
 
